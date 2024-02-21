561a4bf2dc54db18f064e8c6b4a9c7bd0d2150a0
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-21 19:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const output = document.querySelector("#output");
+output.textContent = "null";
+const arr = [1, 2, 3, 4];
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(arr);
+    }, 3000);
+});
+
+promise.then((arr) => {
+    let oddNumbersStr = "";
+
+    setTimeout(() => {
+        arr.filter((element) => {
+            if (element % 2 === 1) {
+                oddNumbersStr += element + " ";
+            }
+        });
+
+        output.textContent = oddNumbersStr;
+    }, 1000);
+
+    return arr;
+}).then((arr) => {
+    let evenNumbersStr = "";
+
+    setTimeout(() => {
+        arr.filter((element) => {
+            if (element % 2 === 0) {
+                evenNumbersStr += element * 2 + " ";
+            }
+        });
+
+        output.textContent += evenNumbersStr; // Use += to append to existing content
+    }, 2000);
+});
